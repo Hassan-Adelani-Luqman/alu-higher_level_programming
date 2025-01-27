94b11bf9908164d1fab74eb140bc61b897d5df13
Hassan-Adelani-Luqman <h.luqman@alustudent.com>
2025-01-27 02:52:04 -0800

    commit

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-function print(arg){
- if(!arg){
- console.log('No argument');
- }else if(arg.length === 1){
- console.log('Argument found')
- }else{
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
+  console.log('No argument');
+} else if (myArgs.length === 1) {
+  console.log('Argument found');
+} else {
   console.log('Arguments found');
- }
-};
+}
